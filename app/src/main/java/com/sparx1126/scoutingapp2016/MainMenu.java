7d8700d5b59ef69423196e93af242474796b6c98
Dan Martin <wifidaniel@gmail.com>
2016-01-16 11:55:48 -0500

    Merged files

--- app/src/main/java/com/sparx1126/scoutingapp2016/MainMenu.java
+++ app/src/main/java/com/sparx1126/scoutingapp2016/MainMenu.java
@@ -1,27 +1,28 @@
 package com.sparx1126.scoutingapp2016;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-import static android.R.layout.simple_spinner_dropdown_item;
-import static android.R.layout.simple_spinner_item;
+import org.gosparx.scouting.aerialassist.DatabaseHelper;
+import org.gosparx.scouting.aerialassist.networking.*;
 
-public class MainMenu extends AppCompatActivity {
+import static org.gosparx.scouting.aerialassist.networking.NetworkHelper.isNetworkAvailable;
 
-    public void addEvents() {
+public class MainMenu extends AppCompatActivity
+{
 
-        Spinner eventPicker = (Spinner) findViewById(R.id.eventPicker);
-        // ArrayAdapter<CharSequence> dataAdapter = ArrayAdapter.createFromResource(this, R.array.spinnerValues, android.R.layout.simple_spinner_dropdown_item);
-        ArrayAdapter<String> dataAdapter;
-        dataAdapter = new ArrayAdapter<String>(this, simple_spinner_item, EventInfo.getEventNames());
-        dataAdapter.setDropDownViewResource(simple_spinner_dropdown_item);
-        eventPicker.setAdapter(dataAdapter);
-    }
+    private SimpleCursorAdapter cursorAdapterRegionalNames;
+    private SimpleCursorAdapter cursorAdapterMatches;
+    private SimpleCursorAdapter cursorAdapterTeams;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +30,8 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        addEvents();
-        }
+        DownloadEventSpinnerDataIfNecessary();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -50,7 +51,91 @@ public class MainMenu extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        else if (id == R.id.action_download)
+        {
+            DownloadEventSpinnerData();
+        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * notify the user about data download
+     * @return message about download
+     */
+    private AlertDialog createDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.downloading_data);
+        builder.setMessage(R.string.please_wait_while_data_downloads);
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                BlueAlliance.getInstance(MainMenu.this).cancelAll();
+                dialogInterface.dismiss();
+            }
+        });
+        return builder.create();
+    }
+
+    /**
+     * check if data needs to be downloaded
+     */
+    private void DownloadEventSpinnerDataIfNecessary()
+    {
+        if(isNetworkAvailable(this) && NetworkHelper.needToLoadEventList(this)) {
+            DownloadEventSpinnerData();
+        }
+        else {
+            SetupEventSpinner();
+        }
+    }
+
+    /**
+     * get data to populate spinner
+     */
+    private void DownloadEventSpinnerData()
+    {
+        final Dialog alert = createDialog();
+        alert.show();
+        BlueAlliance ba = BlueAlliance.getInstance(this);
+        ba.loadEventList(2016, new NetworkCallback() {
+            @Override
+            public void handleFinishDownload(final boolean success) {
+                MainMenu.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (!success)
+                            Toast.makeText(MainMenu.this, "Did not successfully download event list!", Toast.LENGTH_LONG).show();
+                        alert.dismiss();
+                        SetupEventSpinner();
+//                            mNavigationDrawerFragment.updateDrawerData();
+                    }
+                });
+            }
+        });
+    }
+
+/*
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);*/
+
+    /**
+     * creates the spinner with values supplied by BlueAlliance
+     */
+    public void SetupEventSpinner()
+    {
+        DatabaseHelper dbHelper;
+        dbHelper = DatabaseHelper.getInstance(this);
+        Spinner eventPicker = (Spinner)findViewById(R.id.eventPicker);
+        cursorAdapterRegionalNames = new SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                dbHelper.createEventNameCursor(),
+                new String[]{"title"},
+                new int[]{android.R.id.text1}, 0);
+        eventPicker.setAdapter(cursorAdapterRegionalNames);
     }
 }
