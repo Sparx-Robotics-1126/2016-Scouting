59f79bbbff490051af9e41093993b2e54833645d
Dan Martin <wifidaniel@gmail.com>
2016-02-13 12:02:09 -0500

    basic data view implemented; need to check what kind of data we need to show

---
+++ app/src/main/java/com/sparx1126/scoutingapp2016/ViewData.java
@@ -0,0 +1,21 @@
+package com.sparx1126.scoutingapp2016;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ViewData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_data);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
